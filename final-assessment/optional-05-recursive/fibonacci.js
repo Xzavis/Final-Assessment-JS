8f17a9b0878430ba9e8537d99a4a2e87713c5863
Xzavis <xzaabdulmalikibrahim354@gmail.com>
2025-01-20 11:46:27 +0700

    final-assessment/optional-05-recursive/mengerjakan fungsi fibonaci dan bilangan faktorial

--- final-assessment/optional-05-recursive/fibonacci.js
+++ final-assessment/optional-05-recursive/fibonacci.js
@@ -1,5 +1,15 @@
 function fibonacci(n) {
-
+  if (n === 0) {
+    return [0]; // Basis kasus untuk n = 0
+  } else if (n === 1) {
+    return [0, 1]; // Basis kasus untuk n = 1
+  } else {
+    const fibSeries = fibonacci(n - 1); // Rekursi untuk mendapatkan deret sebelumnya
+    fibSeries.push(
+      fibSeries[fibSeries.length - 1] + fibSeries[fibSeries.length - 2]
+    ); // Menambahkan suku berikutnya
+    return fibSeries;
+  }
 }
 
 // Jangan hapus kode di bawah ini!
